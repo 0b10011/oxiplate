4130ac7e6d0ef1df26347a2b207a7bf668048d9e
Brandon Frohs <brandon@19.codes>
2022-11-05 15:45:06 -0400

    Add tests for combinations of if/elseif/else statements

--- tests/if-else.rs
+++ tests/if-else.rs
@@ -4,6 +4,8 @@ use oxiplate::Oxiplate;
 #[oxiplate_inline = "
 {%- if do_this -%}
     This then {{ action }} :D
+{%- else -%}
+    Can't {{ action }} :(
 {%- endif %}"]
 struct Data {
     do_this: bool,
@@ -18,4 +20,14 @@ fn test_if() {
     };
 
     assert_eq!(format!("{}", data), "This then do something :D");
+}
+
+#[test]
+fn test_else() {
+    let data = Data {
+        do_this: false,
+        action: "do something",
+    };
+
+    assert_eq!(format!("{}", data), "Can't do something :(");
 }
--- tests/if-elseif.rs
+++ tests/if-elseif.rs
@@ -4,9 +4,12 @@ use oxiplate::Oxiplate;
 #[oxiplate_inline = "
 {%- if do_this -%}
     This then {{ action }} :D
+{%- elseif do_that -%}
+    That then {{ action }} :D
 {%- endif %}"]
 struct Data {
     do_this: bool,
+    do_that: bool,
     action: &'static str,
 }
 
@@ -14,8 +17,20 @@ struct Data {
 fn test_if() {
     let data = Data {
         do_this: true,
+        do_that: true,
         action: "do something",
     };
 
     assert_eq!(format!("{}", data), "This then do something :D");
+}
+
+#[test]
+fn test_else_if() {
+    let data = Data {
+        do_this: false,
+        do_that: true,
+        action: "do something",
+    };
+
+    assert_eq!(format!("{}", data), "That then do something :D");
 }
