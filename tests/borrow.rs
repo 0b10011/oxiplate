17a42d91ed7268c84a5e2f5a81c6e9d1b5171c92
Brandon Frohs <brandon@19.codes>
2023-06-05 20:44:51 -0400

    remove old comment

--- tests/borrow.rs
+++ tests/borrow.rs
@@ -15,7 +15,6 @@ fn field() {
     let name = "Liv";
     let user = User { name: &name };
     let data = Data { user: &user };
-    // panic!("The problem is the `impl Display for Data {` doesn't specify lifetime like `impl Display for Data<'_>` {`");
 
     assert_eq!(format!("{}", data), "Liv");
 }
