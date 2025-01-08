31b123c0a6e4a8c9e2180d33b828b24a58f769da
Brandon Frohs <brandon@19.codes>
2025-01-08 17:56:01 -0500

    test: merge variable tests into a single file

--- oxiplate-derive/tests/expansion/expected/variables.rs
+++ oxiplate-derive/tests/expansion/expected/variables.rs
@@ -4,12 +4,67 @@ use std::prelude::rust_2021::*;
 #[macro_use]
 extern crate std;
 use oxiplate_derive::Oxiplate;
+#[oxiplate_inline = "{{ message }}"]
+struct Variable {
+    message: &'static str,
+}
+impl ::std::fmt::Display for Variable {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        f.write_fmt(format_args!("{0}", self.message))?;
+        Ok(())
+    }
+}
+extern crate test;
+#[cfg(test)]
+#[rustc_test_marker = "variable"]
+#[doc(hidden)]
+pub const variable: test::TestDescAndFn = test::TestDescAndFn {
+    desc: test::TestDesc {
+        name: test::StaticTestName("variable"),
+        ignore: false,
+        ignore_message: ::core::option::Option::None,
+        source_file: "oxiplate-derive\\tests\\variables.rs",
+        start_line: 10usize,
+        start_col: 4usize,
+        end_line: 10usize,
+        end_col: 12usize,
+        compile_fail: false,
+        no_run: false,
+        should_panic: test::ShouldPanic::No,
+        test_type: test::TestType::IntegrationTest,
+    },
+    testfn: test::StaticTestFn(#[coverage(off)] || test::assert_test_result(variable())),
+};
+fn variable() {
+    let data = Variable {
+        message: "Hello world!",
+    };
+    match (
+        &::alloc::__export::must_use({
+            let res = ::alloc::fmt::format(format_args!("{0}", data));
+            res
+        }),
+        &"Hello world!",
+    ) {
+        (left_val, right_val) => {
+            if !(*left_val == *right_val) {
+                let kind = ::core::panicking::AssertKind::Eq;
+                ::core::panicking::assert_failed(
+                    kind,
+                    &*left_val,
+                    &*right_val,
+                    ::core::option::Option::None,
+                );
+            }
+        }
+    };
+}
 #[oxiplate_inline = "{{ title }} / {{ message }}"]
-struct Data {
+struct Variables {
     title: &'static str,
     message: &'static str,
 }
-impl ::std::fmt::Display for Data {
+impl ::std::fmt::Display for Variables {
     fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
         f.write_fmt(format_args!("{0} / {1}", self.title, self.message))?;
         Ok(())
@@ -25,9 +80,9 @@ pub const variables: test::TestDescAndFn = test::TestDescAndFn {
         ignore: false,
         ignore_message: ::core::option::Option::None,
         source_file: "oxiplate-derive\\tests\\variables.rs",
-        start_line: 11usize,
+        start_line: 26usize,
         start_col: 4usize,
-        end_line: 11usize,
+        end_line: 26usize,
         end_col: 13usize,
         compile_fail: false,
         no_run: false,
@@ -37,7 +92,7 @@ pub const variables: test::TestDescAndFn = test::TestDescAndFn {
     testfn: test::StaticTestFn(#[coverage(off)] || test::assert_test_result(variables())),
 };
 fn variables() {
-    let data = Data {
+    let data = Variables {
         title: "Foo Bar",
         message: "Hello world!",
     };
@@ -66,5 +121,5 @@ fn variables() {
 #[doc(hidden)]
 pub fn main() -> () {
     extern crate test;
-    test::test_main_static(&[&variables])
+    test::test_main_static(&[&variable, &variables])
 }
--- oxiplate-derive/tests/variables.rs
+++ oxiplate-derive/tests/variables.rs
@@ -1,15 +1,30 @@
 use oxiplate_derive::Oxiplate;
 
 #[derive(Oxiplate)]
+#[oxiplate_inline = "{{ message }}"]
+struct Variable {
+    message: &'static str,
+}
+
+#[test]
+fn variable() {
+    let data = Variable {
+        message: "Hello world!",
+    };
+
+    assert_eq!(format!("{data}"), "Hello world!");
+}
+
+#[derive(Oxiplate)]
 #[oxiplate_inline = "{{ title }} / {{ message }}"]
-struct Data {
+struct Variables {
     title: &'static str,
     message: &'static str,
 }
 
 #[test]
 fn variables() {
-    let data = Data {
+    let data = Variables {
         title: "Foo Bar",
         message: "Hello world!",
     };
