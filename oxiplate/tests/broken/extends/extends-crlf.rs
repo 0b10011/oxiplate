79b34c91152c37e4afe14234e01787f44a800692
Brandon Frohs <brandon@19.codes>
2025-01-25 16:27:32 -0500

    test: crlf test with current and fixed output
    the file with the `.fixed` extension should replace the one without once it's fixed

---
+++ oxiplate/tests/broken/extends/extends-crlf.rs
@@ -0,0 +1,12 @@
+use oxiplate_derive::Oxiplate;
+
+#[derive(Oxiplate)]
+#[oxiplate = "extends-with-empty-content-crlf.html.oxip"]
+struct AbsoluteData {}
+
+fn main() {
+    // Intentionally missing the "title" field used by the parent template
+    let data = AbsoluteData {};
+
+    panic!("{data}");
+}
