cc7aa250895ae52e3a366d2b003bf21868fe9a50
Brandon Frohs <brandon@19.codes>
2025-07-13 16:30:41 -0400

    refactor: split ident-related code into separate mod

---
+++ oxiplate-derive/src/syntax/expression/ident.rs
@@ -0,0 +1,120 @@
+use nom::bytes::complete::{tag, take_while1};
+use nom::combinator::{cut, opt, peek};
+use nom::sequence::pair;
+use proc_macro2::{Group, TokenStream};
+use quote::{quote, ToTokens, TokenStreamExt};
+
+use super::{Expression, Res};
+use crate::{Source, State};
+
+pub(crate) fn identifier<'a>(state: &'a State) -> impl Fn(Source) -> Res<Source, Expression> + 'a {
+    |input| {
+        let (input, (ident, parens)) = pair(&ident, opt(tag("()")))(input)?;
+
+        let ident_str = ident.ident;
+        let field = if let Some(parens) = parens {
+            IdentifierOrFunction::Function(ident, parens)
+        } else {
+            IdentifierOrFunction::Identifier(ident)
+        };
+        let is_extending = input.original.is_extending;
+        let is_local = state.local_variables.contains(ident_str);
+
+        Ok((
+            input,
+            Expression::Identifier(
+                field,
+                if is_local {
+                    IdentifierScope::Local
+                } else if is_extending {
+                    IdentifierScope::Data
+                } else {
+                    IdentifierScope::Parent
+                },
+            ),
+        ))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) struct Identifier<'a> {
+    pub ident: &'a str,
+    pub source: Source<'a>,
+}
+
+impl ToTokens for Identifier<'_> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let ident = match self.ident.to_ascii_lowercase().as_str() {
+            keyword @ ("self" | "super") => panic!("{keyword} cannot be a raw identifier"),
+
+            // Keywords from <https://doc.rust-lang.org/reference/keywords.html>.
+            // Prefix with `r#` so Rust will accept them as idents.
+            "abstract" | "as" | "async" | "await" | "become" | "box" | "break" | "const"
+            | "continue" | "crate" | "do" | "dyn" | "else" | "enum" | "extern" | "false"
+            | "final" | "fn" | "for" | "if" | "impl" | "in" | "let" | "loop" | "macro"
+            | "macro_rules" | "match" | "mod" | "move" | "mut" | "override" | "priv" | "pub"
+            | "ref" | "return" | "static" | "struct" | "trait" | "true" | "try" | "type"
+            | "typeof" | "union" | "unsafe" | "unsized" | "use" | "virtual" | "where" | "while"
+            | "yield" => syn::Ident::new_raw(self.ident, self.source.span()),
+
+            _ => syn::Ident::new(self.ident, self.source.span()),
+        };
+
+        tokens.append_all(quote! { #ident });
+    }
+}
+
+pub(crate) fn ident(input: Source) -> Res<Source, Identifier> {
+    // Ignore if it starts with a number
+    let (input, _) = peek(take_while1(
+        |char: char| matches!(char, 'a'..='z' | 'A'..='Z' | '_'),
+    ))(input)?;
+
+    let (input, ident) = cut(take_while1(
+        |char: char| matches!(char, 'a'..='z' | 'A'..='Z' | '0'..='9' | '_'),
+    ))(input)?;
+    Ok((
+        input,
+        Identifier {
+            ident: ident.as_str(),
+            source: ident,
+        },
+    ))
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) enum IdentifierOrFunction<'a> {
+    Identifier(Identifier<'a>),
+    Function(Identifier<'a>, Source<'a>),
+}
+impl ToTokens for IdentifierOrFunction<'_> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        match self {
+            IdentifierOrFunction::Identifier(identifier) => {
+                tokens.append_all(quote! { #identifier });
+            }
+            IdentifierOrFunction::Function(identifier, parens) => {
+                let span = parens.span();
+                let mut parens =
+                    Group::new(proc_macro2::Delimiter::Parenthesis, TokenStream::new());
+                parens.set_span(span);
+
+                tokens.append_all(quote! { #identifier #parens });
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum IdentifierScope {
+    Local,
+    Parent,
+    Data,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct IdentField<'a> {
+    parents: Vec<Identifier<'a>>,
+    ident_or_function: IdentifierOrFunction<'a>,
+    scope: IdentifierScope,
+}
--- oxiplate-derive/src/syntax/expression/mod.rs
+++ oxiplate-derive/src/syntax/expression/mod.rs
@@ -1,7 +1,7 @@
 use nom::branch::alt;
 use nom::bytes::complete::{tag, take, take_while, take_while1};
 use nom::character::complete::char;
-use nom::combinator::{cut, fail, not, opt, peek};
+use nom::combinator::{cut, fail, not, opt};
 use nom::error::context;
 use nom::multi::{many0, many1, many_till};
 use nom::sequence::{pair, tuple};
@@ -9,96 +9,17 @@ use proc_macro2::{Group, TokenStream};
 use quote::{quote, quote_spanned, ToTokens, TokenStreamExt};
 use syn::token::Dot;
 
+use self::ident::{IdentifierOrFunction, IdentifierScope};
 use super::template::whitespace;
 use super::Res;
 use crate::syntax::item::tag_end;
 use crate::{Source, State};
 
+mod ident;
 mod keyword;
-pub(super) use keyword::{keyword, Keyword};
 
-#[derive(Debug, PartialEq, Eq)]
-pub(crate) struct Identifier<'a> {
-    pub ident: &'a str,
-    pub source: Source<'a>,
-}
-
-impl ToTokens for Identifier<'_> {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        let ident = match self.ident.to_ascii_lowercase().as_str() {
-            keyword @ ("self" | "super") => panic!("{keyword} cannot be a raw identifier"),
-
-            // Keywords from <https://doc.rust-lang.org/reference/keywords.html>.
-            // Prefix with `r#` so Rust will accept them as idents.
-            "abstract" | "as" | "async" | "await" | "become" | "box" | "break" | "const"
-            | "continue" | "crate" | "do" | "dyn" | "else" | "enum" | "extern" | "false"
-            | "final" | "fn" | "for" | "if" | "impl" | "in" | "let" | "loop" | "macro"
-            | "macro_rules" | "match" | "mod" | "move" | "mut" | "override" | "priv" | "pub"
-            | "ref" | "return" | "static" | "struct" | "trait" | "true" | "try" | "type"
-            | "typeof" | "union" | "unsafe" | "unsized" | "use" | "virtual" | "where" | "while"
-            | "yield" => syn::Ident::new_raw(self.ident, self.source.span()),
-
-            _ => syn::Ident::new(self.ident, self.source.span()),
-        };
-
-        tokens.append_all(quote! { #ident });
-    }
-}
-
-pub(super) fn ident(input: Source) -> Res<Source, Identifier> {
-    // Ignore if it starts with a number
-    let (input, _) = peek(take_while1(
-        |char: char| matches!(char, 'a'..='z' | 'A'..='Z' | '_'),
-    ))(input)?;
-
-    let (input, ident) = cut(take_while1(
-        |char: char| matches!(char, 'a'..='z' | 'A'..='Z' | '0'..='9' | '_'),
-    ))(input)?;
-    Ok((
-        input,
-        Identifier {
-            ident: ident.as_str(),
-            source: ident,
-        },
-    ))
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub(crate) enum IdentifierOrFunction<'a> {
-    Identifier(Identifier<'a>),
-    Function(Identifier<'a>, Source<'a>),
-}
-impl ToTokens for IdentifierOrFunction<'_> {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        match self {
-            IdentifierOrFunction::Identifier(identifier) => {
-                tokens.append_all(quote! { #identifier });
-            }
-            IdentifierOrFunction::Function(identifier, parens) => {
-                let span = parens.span();
-                let mut parens =
-                    Group::new(proc_macro2::Delimiter::Parenthesis, TokenStream::new());
-                parens.set_span(span);
-
-                tokens.append_all(quote! { #identifier #parens });
-            }
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum IdentifierScope {
-    Local,
-    Parent,
-    Data,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct IdentField<'a> {
-    parents: Vec<Identifier<'a>>,
-    ident_or_function: IdentifierOrFunction<'a>,
-    scope: IdentifierScope,
-}
+pub(super) use self::ident::{ident, identifier, Identifier};
+pub(super) use self::keyword::{keyword, Keyword};
 
 #[derive(Debug, PartialEq, Eq)]
 pub(crate) struct Field<'a> {
@@ -356,35 +277,6 @@ pub(super) fn expression<'a>(
         )(input)?;
 
         Ok((input, ExpressionAccess { expression, fields }))
-    }
-}
-
-fn identifier<'a>(state: &'a State) -> impl Fn(Source) -> Res<Source, Expression> + 'a {
-    |input| {
-        let (input, (ident, parens)) = pair(&ident, opt(tag("()")))(input)?;
-
-        let ident_str = ident.ident;
-        let field = if let Some(parens) = parens {
-            IdentifierOrFunction::Function(ident, parens)
-        } else {
-            IdentifierOrFunction::Identifier(ident)
-        };
-        let is_extending = input.original.is_extending;
-        let is_local = state.local_variables.contains(ident_str);
-
-        Ok((
-            input,
-            Expression::Identifier(
-                field,
-                if is_local {
-                    IdentifierScope::Local
-                } else if is_extending {
-                    IdentifierScope::Data
-                } else {
-                    IdentifierScope::Parent
-                },
-            ),
-        ))
     }
 }
 
