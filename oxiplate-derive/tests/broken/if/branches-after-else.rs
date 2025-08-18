3b2513a4d8302d8aefe59359b76b45e7c9f5d834
Brandon Frohs <brandon@19.codes>
2025-08-18 12:49:40 -0400

    test: added test for extra `else` statements in `if` statement

---
+++ oxiplate-derive/tests/broken/if/branches-after-else.rs
@@ -0,0 +1,25 @@
+use oxiplate_derive::Oxiplate;
+
+#[derive(Oxiplate)]
+#[oxiplate_inline("
+{% if foo %}
+    foo
+{% else %}
+    bar
+{% else %}
+    hello
+{% else %}
+    world
+{% elseif foo %}
+    hello
+{% elseif foo %}
+    world
+{% endif %}
+{% else %}")]
+struct Data {
+    foo: bool,
+}
+
+fn main() {
+    print!("{}", Data { foo: true });
+}
