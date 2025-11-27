02839dde7a2e0f5e826e294650cbf5565172b1d2
Brandon Frohs <brandon@19.codes>
2025-11-27 15:09:46 -0500

    test: added tests for text/writs in extends

---
+++ oxiplate-derive/tests/broken/extends/static-text.rs
@@ -0,0 +1,9 @@
+use oxiplate_derive::Oxiplate;
+
+#[derive(Oxiplate)]
+#[oxiplate_inline(r#"{% extends "multiple-blocks.html.oxip" %}hello"#)]
+struct Static;
+
+fn main() {
+    print!("{}", Static);
+}
