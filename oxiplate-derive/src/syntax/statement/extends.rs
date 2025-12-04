edb23fa761908c619e0d07025f6d7aee2ab40c1c
Brandon Frohs <brandon@19.codes>
2025-12-03 19:24:45 -0500

    refactor: removed old extends code that's no longer needed

--- oxiplate-derive/src/syntax/statement/extends.rs
+++ oxiplate-derive/src/syntax/statement/extends.rs
@@ -7,7 +7,7 @@ use nom::combinator::cut;
 use nom::error::context;
 use proc_macro2::TokenStream;
 use quote::{TokenStreamExt, quote, quote_spanned};
-use syn::{GenericArgument, Ident, LitStr};
+use syn::LitStr;
 
 use super::super::Res;
 use super::super::expression::keyword;
@@ -18,7 +18,6 @@ use crate::{Source, State};
 
 #[derive(Debug)]
 pub struct Extends<'a> {
-    is_extending: bool,
     blocks: HashMap<&'a str, (Template<'a>, Option<Template<'a>>)>,
     path: Source<'a>,
     template: Template<'a>,
@@ -77,44 +76,6 @@ impl<'a> Extends<'a> {
         let span = self.path.span();
         let path = LitStr::new(self.path.as_str(), span);
 
-        // FIXME: Should also include local vars here I think
-        let mut block_generics = vec![];
-        let mut block_constraints = vec![];
-        let mut block_definitions = vec![];
-        let mut block_values = vec![];
-        let mut generic_name_i = 0;
-        for item in &self.template.0 {
-            if let Item::Statement(Statement {
-                kind: StatementKind::Block(block),
-                ..
-            }) = item
-            {
-                // Build generic name and bounds
-                generic_name_i += 1;
-                let generic_name = Ident::new(&format!("Block{generic_name_i}"), span);
-                let constraint: GenericArgument = syn::parse_quote_spanned!(span=>
-                    #generic_name: Fn(fn(f: &mut dyn Write) -> ::std::fmt::Result, &mut dyn Write) -> ::std::fmt::Result
-                );
-                let block_name = &block.name;
-
-                block_definitions.push(quote_spanned! {span=>
-                    #block_name: &'a #generic_name
-                });
-                block_generics.push(generic_name);
-                block_constraints.push(constraint);
-
-                if self.blocks.contains_key(block.name.ident) {
-                    block_values.push(quote_spanned! {span=>
-                        #block_name: &self.#block_name
-                    });
-                } else {
-                    block_values.push(quote_spanned! {span=>
-                        #block_name: &#block_name
-                    });
-                }
-            }
-        }
-
         #[cfg(feature = "oxiplate")]
         let oxiplate = quote_spanned! {span=> ::oxiplate::Oxiplate };
         #[cfg(not(feature = "oxiplate"))]
@@ -123,26 +84,10 @@ impl<'a> Extends<'a> {
         let (template, _template_length) = &self.template.to_tokens(state);
         let mut tokens: TokenStream = quote! { #template };
 
-        let template_to_extend = if self.is_extending {
-            quote_spanned! {span=>
-                #[derive(#oxiplate)]
-                #[oxiplate_extends = #path]
-                struct ExtendingTemplate<'a, #(#block_generics),*>
-                where #(#block_constraints),*
-                {
-                    #(#block_definitions,)*
-                }
-            }
-        } else {
-            quote_spanned! {span=>
-                #[derive(#oxiplate)]
-                #[oxiplate_extends = #path]
-                struct Template<'a, #(#block_generics),*>
-                where #(#block_constraints),*
-                {
-                    #(#block_definitions,)*
-                }
-            }
+        let template_to_extend = quote_spanned! {span=>
+            #[derive(#oxiplate)]
+            #[oxiplate_extends = #path]
+            struct Template {}
         };
 
         let mut block_stack = state.blocks.clone();
@@ -183,15 +128,12 @@ pub(super) fn parse_extends(input: Source) -> Res<Source, Statement> {
     ))
     .parse(input)?;
 
-    let is_extending = input.original.is_extending;
-
     let source = extends_keyword.0;
 
     Ok((
         input,
         Statement {
             kind: Extends {
-                is_extending,
                 blocks: HashMap::new(),
                 path,
                 template: Template(vec![]),
