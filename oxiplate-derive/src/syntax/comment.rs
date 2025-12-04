25570c17e119a0a93a18a3fb6db1568bc497a497
Brandon Frohs <brandon@19.codes>
2025-12-04 00:36:11 -0500

    fix: improved error message when a comment tag is not closed

--- oxiplate-derive/src/syntax/comment.rs
+++ oxiplate-derive/src/syntax/comment.rs
@@ -1,6 +1,7 @@
 use nom::Parser as _;
 use nom::branch::alt;
 use nom::bytes::complete::{tag, take_till1};
+use nom::error::context;
 use nom::multi::many_till;
 
 use super::item::tag_end;
@@ -8,14 +9,17 @@ use super::{Item, Res};
 use crate::Source;
 
 pub(super) fn comment(input: Source) -> Res<Source, (Item, Option<Item>)> {
-    let (input, (_comment, (trailing_whitespace, _close_tag))) = many_till(
-        alt((
-            take_till1(|char| char == '-' || char == '_' || char == '#'),
-            tag("-"),
-            tag("_"),
-            tag("#"),
-        )),
-        tag_end("#}"),
+    let (input, (_comment, (trailing_whitespace, _close_tag))) = context(
+        "Expected comment text followed by `#}`",
+        many_till(
+            alt((
+                take_till1(|char| char == '-' || char == '_' || char == '#'),
+                tag("-"),
+                tag("_"),
+                tag("#"),
+            )),
+            tag_end("#}"),
+        ),
     )
     .parse(input)?;
 
