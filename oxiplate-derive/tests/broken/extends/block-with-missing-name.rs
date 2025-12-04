abe16151006c27e98a3e2a76dc675c979826fee6
Brandon Frohs <brandon@19.codes>
2025-12-04 00:27:38 -0500

    test: added test for missing block name

---
+++ oxiplate-derive/tests/broken/extends/block-with-missing-name.rs
@@ -0,0 +1,12 @@
+use oxiplate_derive::Oxiplate;
+
+#[derive(Oxiplate)]
+#[oxiplate_inline(r#"{% extends "extends.html.oxip" %}{% block %}"#)]
+struct Data {
+    title: &'static str,
+    message: &'static str,
+}
+
+fn main() {
+    print!("{}", Data { title: "Title", message: "Message" });
+}
