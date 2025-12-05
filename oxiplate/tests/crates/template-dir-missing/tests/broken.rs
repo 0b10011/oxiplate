c332f10926cc75c900810f6f2e793dca730e9da3
Brandon Frohs <brandon@19.codes>
2025-12-05 03:04:40 -0500

    test: added tests for several template dir failures

---
+++ oxiplate/tests/crates/template-dir-missing/tests/broken.rs
@@ -0,0 +1,14 @@
+#[test]
+fn broken() {
+    unsafe {
+        std::env::set_var(
+            "CARGO_MANIFEST_DIR_OVERRIDE",
+            std::env::var("CARGO_MANIFEST_DIR").unwrap(),
+        );
+        std::env::set_var("OXIP_TEMPLATE_DIR", "missing-dir");
+    }
+
+    let tests = trybuild::TestCases::new();
+
+    tests.compile_fail("tests/broken/broken.rs");
+}
---
+++ oxiplate/tests/crates/template-dir-symlink/tests/broken/broken.rs
@@ -0,0 +1,12 @@
+use oxiplate::{Oxiplate, Render};
+
+#[derive(Oxiplate)]
+#[oxiplate = "external.html.oxip"]
+struct Data;
+
+fn main() {
+    assert_eq!(
+        Data.render().unwrap(),
+        "Hello world!"
+    );
+}
