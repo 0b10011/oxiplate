68075f3d9c82e68a7aaff2963ba24048eaf5a5bc
Brandon Frohs <brandon@19.codes>
2025-12-10 01:16:46 -0500

    test: added test for malformed struct value

---
+++ oxiplate-derive/tests/broken/if/if-let-bad-field-value.rs
@@ -0,0 +1,20 @@
+use oxiplate_derive::Oxiplate;
+
+#[derive(Oxiplate)]
+#[oxiplate_inline(
+    r#"
+{%- if let Data { value: :( } = data -%}
+    Data found.
+{%- endif -%}
+"#
+)]
+struct Data {
+    value: Option<&'static str>,
+}
+
+fn main() {
+    assert_eq!(
+        "Data found.",
+        format!("{}", Data { value: None })
+    );
+}
