1f2dbd2b1cd9fb8374b2ec71edec3801f3465730
Brandon Frohs <brandon@19.codes>
2026-02-08 02:07:10 -0500

    test: removed nearly duplicate test
    config is now tested thoroughly via auto-generated crates

--- oxiplate/tests/crates/config-malformed/tests/broken/malformed-config.rs
+++
@@ -1,9 +0,0 @@
-use oxiplate::{Oxiplate, Render};
-
-#[derive(Oxiplate)]
-#[oxiplate_inline(html: "hello world")]
-struct Data;
-
-fn main() {
-    assert_eq!(Data.render().unwrap(), "Hello world!");
-}
